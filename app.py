fea0d5dc0a9a5cb95fec3cf43a1a2d90e5dbdeb1
Muhammad Nouman <noumannow@gmail.com>
2023-05-31 12:27:47 +0500

    Edit Method

--- app.py
+++ app.py
@@ -52,3 +52,27 @@ def create():
             conn.close()
             return redirect(url_for('index'))
     return render_template('create.html')
+
+
+. . .
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
