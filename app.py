9575cb63bec1e58d7c166cd1dc104f45ca79eb99
Muhammad Nouman <noumannow@gmail.com>
2023-05-31 12:31:46 +0500

    Modified edit and added the delete method

--- app.py
+++ app.py
@@ -54,8 +54,6 @@ def create():
     return render_template('create.html')
 
 
-. . .
-
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
     post = get_post(id)
@@ -76,3 +74,14 @@ def edit(id):
             return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
