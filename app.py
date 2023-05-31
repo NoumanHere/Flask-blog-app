ae90683f121df1a81e7cd33ac636ab557c74c08d
Muhammad Nouman <noumannow@gmail.com>
2023-05-31 12:22:45 +0500

    Modified create method

--- app.py
+++ app.py
@@ -38,4 +38,17 @@ def post(post_id):
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
     return render_template('create.html')
